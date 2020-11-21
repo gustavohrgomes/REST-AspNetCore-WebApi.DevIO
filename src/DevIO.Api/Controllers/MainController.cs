80486b17f84a24f86c588d1565d96c3bf4724d91
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-11-21 20:39:29 -0300

    Create first two controllers

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // Validação de notificações de erro
+
+        // Validação de model state
+
+        // validação da operação de negócios
+    }
+}
