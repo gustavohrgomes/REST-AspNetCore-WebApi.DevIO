ba3fffeeb465d09b7396b942c8b60daa05cf1280
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-11-21 20:41:04 -0300

    Configuring Dependency Injection system & startup class

---
+++ src/DevIO.Api/Configurations/DependecyInjectionConfig.cs
@@ -0,0 +1,22 @@
+﻿using DevIO.Business.Intefaces;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Configurations
+{
+    public static class DependecyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependecies(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+
+            return services;
+        }
+    }
+}
