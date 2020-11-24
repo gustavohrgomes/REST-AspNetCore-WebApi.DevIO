2c907f69e9fe4ea9c2d2e9e431bd8b9d7818b9c5
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-11-23 23:19:46 -0300

    Resolving Dependicies

--- src/DevIO.Api/Configurations/DependecyInjectionConfig.cs
+++ src/DevIO.Api/Configurations/DependecyInjectionConfig.cs
@@ -1,4 +1,6 @@
 ﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,7 +16,13 @@ namespace DevIO.Api.Configurations
         public static IServiceCollection ResolveDependecies(this IServiceCollection services)
         {
             services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+            services.AddScoped<IFornecedorService, FornecedorService>();
 
             return services;
         }
--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 
 namespace DevIO.Api
 {
@@ -29,7 +30,9 @@ namespace DevIO.Api
 
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options => 
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
             services.ResolveDependecies();
         }
